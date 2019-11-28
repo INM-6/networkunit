8977ebd740cf915ff1e43ab124f63a9db6d58adb
Aitor <a.morales-gregorio@fz-juelich.de>
2019-11-28 10:29:20 +0100

    remove full dict from nest_simulation class, some of the parameters are inter-dependent. Add a check that none of the 'illegal' parameters is set. Standardization of this should wait until NEST issue [1350](https://github.com/nest/nest-simulator/issues/1350)

--- networkunit/models/model_nest_simulation.py
+++ networkunit/models/model_nest_simulation.py
@@ -1,5 +1,4 @@
 import sciunit
-import quantities as pq
 from .backends import available_backends
 try:
     import nest
@@ -22,54 +21,26 @@ class nest_simulation(sciunit.models.RunnableModel):
         if model_params is not None:
             self.model_params.update(model_params)
 
-        # Default kernel status `nest.GetKernelStatus()` on NEST 2.18.0
-        default_run_params = {'adaptive_spike_buffers': True,
-                              'adaptive_target_buffers': True,
-                              'buffer_size_secondary_events': 0,
-                              'buffer_size_spike_data': 2,
-                              'buffer_size_target_data': 2,
-                              'data_path': '',
-                              'data_prefix': '',
-                              'dict_miss_is_error': True,
-                              'grng_seed': 0,
-                              'growth_factor_buffer_spike_data': 1.5,
-                              'growth_factor_buffer_target_data': 1.5,
-                              'keep_source_table': True,
-                              'local_num_threads': 1,
-                              'local_spike_counter': 0,
-                              'max_buffer_size_spike_data': 8388608,
-                              'max_buffer_size_target_data': 16777216,
-                              'max_num_syn_models': 512,
-                              'max_delay': 0.1,
-                              'min_delay': 0.1,
-                              'ms_per_tic': 0.001,
-                              'network_size': 1,
-                              'num_connections': 0,
-                              'num_processes': 1,
-                              'off_grid_spiking': False,
-                              'overwrite_files': False,
-                              'print_time': False,
-                              'resolution': 0.1,
-                              'rng_seeds': (1,),
-                              'sort_connections_by_source': True,
-                              'structural_plasticity_synapses': {},
-                              'structural_plasticity_update_interval': 1000,
-                              'T_max': 1152921504606846.8,
-                              'T_min': -1152921504606846.8,
-                              'tics_per_ms': 1000.0,
-                              'tics_per_step': 100,
-                              'time': 0.0,
-                              'time_collocate': 0.0,
-                              'time_communicate': 0.0,
-                              'to_do': 0,
-                              'total_num_virtual_procs': 1,
-                              'use_wfr': True,
-                              'wfr_comm_interval': 1.0,
-                              'wfr_interpolation_order': 3,
-                              'wfr_max_iterations': 15,
-                              'wfr_tol': 0.0001}
-
-        self.set_default_run_params(**default_run_params)
+        # # Default kernel status `nest.GetKernelStatus()`
+        # nest.ResetKernel()
+        # default_run_params = nest.GetKernelStatus()
+        #
+        # # Remove keys that could raise an error while setting them
+        # non_settable_params = ['T_max',
+        #                        'T_min',
+        #                        'tics_per_step',
+        #                        'time_collocate',
+        #                        'time_communicate',
+        #                        'to_do',
+        #                        'min_delay',
+        #                        'max_delay']
+        # for key in non_settable_params:
+        #     try:
+        #         default_run_params.pop(key)
+        #     except KeyError:
+        #         print(key + ' was not in the default_run_params dict')
+        #
+        # self.set_default_run_params(**default_run_params)
         return None
 
     def init_simulation(self):
