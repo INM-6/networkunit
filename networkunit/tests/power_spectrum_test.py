6af47e17bceb7579efed70d158a90b36e8c3095c
Robin <robin.gutzen@live.de>
2021-02-24 17:45:27 +0100

    improve power spectrum test

--- networkunit/tests/power_spectrum_test.py
+++ networkunit/tests/power_spectrum_test.py
@@ -3,25 +3,24 @@ from networkunit.capabilities.ProducesSpikeTrains import ProducesSpikeTrains
 from elephant.statistics import time_histogram
 from elephant.spectral import welch_psd
 from elephant.signal_processing import zscore
-from networkunit.plots.power_spectral_density import power_spectral_density
+# from networkunit.plots.power_spectral_density import power_spectral_density
 import numpy as np
 import quantities as pq
 from inspect import signature
+
 
 class power_spectrum_test(two_sample_test):
     """
     Test to compare the power spectral density of a set of spiking neurons
     in a network.
-    The statistical testing method needs to be set in form of a
-    sciunit.Score as score_type.
-    Parameters are passed on to elephant.spectral.welch_psd
+    All spiketrains need to have the same t_start and t_stop.
+    Parameters are passed on to elephant.spectral.welch_psd()
     """
 
     required_capabilities = (ProducesSpikeTrains,)
 
-    name = 'Power-Spectrum Test'
-    default_params = {'freq_res': 1,
-                      'binszie': 5*pq.ms,
+    default_params = {'frequency_resolution': 2.5,
+                      'binsize': 10*pq.ms,
                       'psd_precision': 0.0001
                       }
 
@@ -31,24 +30,36 @@ class power_spectrum_test(two_sample_test):
             if kwargs:
                 self.params.update(kwargs)
 
-            # calculating population activity
-            spiketrains = model.produce_spiketrains(**self.params)
-            asignal = time_histogram(spiketrains,
-                                     binsize=self.params['binsize'])
-            zscore(asignal, inplace=True)
+            spiketrains_list = model.produce_grouped_spiketrains(**self.params)
+            psd_samples = []
 
-            # calculating power spectrum with params
-            psd_sig = signature(welch_psd)[0]
-            psd_params = dict((k, self.params[k]) for k in
-                              psd_sig.parameters.keys() if k in self.params)
-            freqs, psd = welch_psd(asignal, **psd_params)
-            psd /= (np.mean(psd)*len(freqs))
-            breakpoint()
-
-            # PSD values -> PSD samples
-            factor = 1/self.params['psd_precision']
-            psd_samples = np.array(([f]*int(np.round(p*factor))
-                                    for f,p in zip(freqs, psd))).flatten()
+            for spiketrains in spiketrains_list:
+                psd = self.spiketrain_psd(spiketrains)
+                psd_samples.append(self.psd_to_samples(freqs, psd))
 
             self.set_prediction(model, psd_samples)
         return psd_samples
+
+
+    def spiketrains_psd(self, spiketrains):
+        if not (type(spiketrains) == list) \
+        or not type(spiketrains[0]) == neo.SpikeTrain:
+            raise TypeError("Input must be a list of neo.Spiketrain obejects.")
+
+        asignal = time_histogram(spiketrains,
+                                 binsize=self.params['binsize'])
+        zscore(asignal, inplace=True)
+
+        psd_sig = signature(welch_psd)[0]
+        psd_params = dict((k, self.params[k]) for k in
+                          psd_sig.parameters.keys() if k in self.params)
+        freqs, psd = welch_psd(asignal, **psd_params)
+        # Adjust so the integral over f is 1
+        psd /= (np.mean(psd)*self.params['frequency_resolution'])
+        return psd
+
+
+    def psd_to_samples(self, freqs, psd):
+        factor = 1/self.params['psd_precision']
+        psd_factors = np.round(psd*factor).astype(int)
+        return np.repeat(freqs, psd_factors)
