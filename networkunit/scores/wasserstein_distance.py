cbf411e2c6f4e6266874da6fa5c0c32a48c9120e
Robin <robin.gutzen@live.de>
2022-04-26 19:02:33 +0200

    adapt wasserstein distance for non joint-test use

--- networkunit/scores/wasserstein_distance.py
+++ networkunit/scores/wasserstein_distance.py
@@ -28,6 +28,11 @@ class wasserstein_distance(sciunit.Score):
             and the mean and std of the full array are used for normalization.
 
         """
+        if type(observation) == list:
+            observation = np.array(observation).reshape((1,len(observation)))
+        if type(prediction) == list:
+            prediction = np.array(prediction).reshape((1,len(prediction)))
+
         if observation.shape[0] == prediction.shape[0]:
             ndim = observation.shape[0]
         else:
